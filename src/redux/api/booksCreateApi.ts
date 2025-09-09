7ec4d03d23178b3c8ab3dbe83fce2c9ce416af8d
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-09 11:25:41 +0600

    books create api

---
+++ src/redux/api/booksCreateApi.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const booksApi = createApi({
+  reducerPath: "booksApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  endpoints: (build) => ({
+    getBooks: build.query({
+      query: () => "items",
+    }),
+  }),
+});
+
+export const { useGetBooksQuery } = booksApi;
