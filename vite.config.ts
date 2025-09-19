0bb396b9fd9d1d901dd3e2748023ee52b6ad4b3b
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-19 23:52:24 +0600

    vite confi

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    // Increase the chunk size warning limit to 2 MB
-    chunkSizeWarningLimit: 2000,
   },
 });
